20270f0f92f51ec3a6c58f1580474e46d0c51076
adxell <adxell02@gmail.com>
2022-11-10 23:59:43 -0500

    config row height in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number }= { 1: 400, 3: 335, 4: 350 }
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cols = 3
   category: string | undefined
+
+  rowHeight = ROWS_HEIGHT[this.cols]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onColumnsCountChange(colsNum: number): void{
-    this.cols = colsNum
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols]
   }
 
   onShowCategory(category: string): void {
