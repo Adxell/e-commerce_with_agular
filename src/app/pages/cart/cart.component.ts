36cd3e054a16f950bcb1905013f990ca9110411a
adxell <adxell02@gmail.com>
2022-12-21 21:54:20 -0500

    feat: use method from from servie card

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,50 +10,77 @@ import { Cart, CartItem } from 'src/app/models/cart.model';
 })
 export class CartComponent implements OnInit {
 
-  cart: Cart = { items: [
-    {
-      id: 1,
-      name: "snack",
-      price: 150,
-      product: 'https://via.placeholder.com/150',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: "snack",
-      price: 150,
-      product: 'https://via.placeholder.com/150',
-      quantity: 5
-    },
-    {
-      id: 3,
-      name: "snack",
-      price: 150,
-      product: 'https://via.placeholder.com/150',
-      quantity: 30
-    },
-]}
+  cart: Cart = {
+    items: [
+      {
+        id: 1,
+        name: "snack",
+        price: 150,
+        product: 'https://via.placeholder.com/150',
+        quantity: 10
+      },
+      {
+        id: 2,
+        name: "snack",
+        price: 150,
+        product: 'https://via.placeholder.com/150',
+        quantity: 5
+      },
+      {
+        id: 3,
+        name: "snack",
+        price: 150,
+        product: 'https://via.placeholder.com/150',
+        quantity: 30
+      },
+    ]
+  }
 
   dataSource: Array<CartItem> = []
 
   displayColumns: Array<string> = [
-    'product', 
-    'name', 
-    'price', 
-    'quantity', 
+    'product',
+    'name',
+    'price',
+    'quantity',
     'total',
     'action'
   ]
-  constructor() { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items
+    })
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items
-    .map((item) => item.price * item.quantity)
-    .reduce((prev, current) => prev + current, 0)
+    return this.cartService.getTotal(items)
   }
 
+  onClearCart(): void {
+    this.cartService.clearCart()
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item)
+  }
+  onAddQuatity(item: CartItem): void {
+    this.cartService.addtoCart(item)
+  }
+  onRemoveQuatity(item: CartItem): void {
+    this.cartService.removeQuantity(item)
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async (res: any) => {
+      let stripe = await loadStripe('pk_test_51MHMGSB90ErkMxt0HnjrLdYF7z8t2eDhR94yxPIbLHlQUBvKVmCiDhNAaRyn5bFTC4YFmxQiJbYgRVh4AegCMmrh00RQ5dKwT7')
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
+  }
 }
