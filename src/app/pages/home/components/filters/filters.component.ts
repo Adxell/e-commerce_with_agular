a63c52a4b345efd0bf6950adedc435c2981994bd
adxell <adxell02@gmail.com>
2022-12-21 21:57:25 -0500

    feat: use service from store

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,22 +1,32 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html',
 })
-export class FiltersComponent implements OnInit {
-  
-  @Output() showCategory = new EventEmitter<string>()
-  
-  categories = ['shoes', 'sports'];
+export class FiltersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Output() showCategory = new EventEmitter<string>()
+  categoriesSubcription: Subscription | undefined;
+  categories: Array<string> | undefined;
+
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
+    this.storeService.getAllCategory().subscribe((resposne) => {
+      this.categories = resposne;
+    })
   }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category)
+  }
+  ngOnDestroy(): void {
+    if (this.categoriesSubcription) {
+      this.categoriesSubcription.unsubscribe()
+    }
   }
 
 }
